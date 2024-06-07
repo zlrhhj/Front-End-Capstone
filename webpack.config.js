e4bf942fb2c872fcf5524bc04eddc1086b1ac338
Liru Zhao <95094916+zlrhhj@users.noreply.github.com>
2024-06-07 10:49:17 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const ReactRefreshPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
 
 module.exports = {
   mode: "development",
