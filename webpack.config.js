6eb7eb8d058bac2d673bf9c17d9df020d0b48aa3
Liru Zhao <95094916+zlrhhj@users.noreply.github.com>
2024-06-07 10:48:49 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -34,25 +34,4 @@ module.exports = {
   },
 };
 
-/**
- *
- * NOTE: About React Fast Refresh:
- *
- * You can enable fast-refresh for this project by enabling the
- * commented lines of code in this file, above and in .babelrc
- *
- * This will change the build so that the client is served by
- * webpack-dev-server and, as such, it should not be loaded via
- * the Express server:
- *
- * - The client will be viewable at http://localhost:8080/
- * - All API requests being sent by the client will be
- *   automatically routed to http://localhost:3000
- * - Since API requests are proxied, the server must still be
- *   running for the React app to compile and run correctly.
- *
- * Since that last requirement _may_ be prohibitive to
- * completing the assigned tasks, the fast-refresh
- * option has been disabled by default.
- *
- */
+
